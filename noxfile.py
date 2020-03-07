6dc75c4c2b4de59ef06fc70a1aeec5b52d03b641
Scott Burns <sburns@centercitypcs.org>
2020-03-07 10:36:39 -0500

    Use Black for code formatting

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 import nox
 
+nox.options.sessions = "lint", "tests"
+
 locations = "ps_gsuite_sync", "tests", "noxfile.py"
 
 
@@ -13,5 +15,12 @@ def tests(session):
 @nox.session
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
+
+
+@nox.session
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
