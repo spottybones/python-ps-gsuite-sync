accfe8a37b7d39921ee110c8e09c3bc17c058160
Scott Burns <sburns@centercitypcs.org>
2020-03-07 10:53:55 -0500

    Add bugbear for bug hunting

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ def tests(session):
 @nox.session
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
     session.run("flake8", *args)
 
 
