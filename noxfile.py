d656aec1f7a2ac775cc175af4b0c0e62b6ffbd0a
Scott Burns <sburns@centercitypcs.org>
2020-03-07 10:29:24 -0500

    Add nox session for linting with flake8

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,17 @@
 import nox
 
+locations = "ps_gsuite_sync", "tests", "noxfile.py"
+
 
 @nox.session
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
