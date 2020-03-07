bf5e89633517320ca02024f6b509a3e4431aa5fd
Scott Burns <sburns@centercitypcs.org>
2020-03-06 20:40:25 -0500

    Test call to console.main

--- tests/test_ps_gsuite_sync.py
+++ tests/test_ps_gsuite_sync.py
@@ -1,5 +1,19 @@
+import click.testing
+import pytest
+
 from ps_gsuite_sync import __version__
+from ps_gsuite_sync import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
